e56a108f33e46c07a7ea8851c8b3a7d2c024ab38
Kai Yao <kaibyao@users.noreply.github.com>
2025-10-20 13:40:16 -0500

    moved page header to own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { SelectAdvocate } from "@/db/schema";
 import "@radix-ui/themes/styles.css";
 import "./globals.css";
+import { AdvocatesPageHeader } from "@/app/_components/AdvocatesPageHeader";
 
 export default function Home() {
   const [advocates, setAdvocates] = useState<SelectAdvocate[]>([]);
@@ -47,52 +48,54 @@ export default function Home() {
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
-      </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr key={advocate.id}>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s: string) => (
-                    <div key={s}>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </main>
+    <>
+      <AdvocatesPageHeader />
+      <main style={{ margin: "24px" }}>
+        <br />
+        <br />
+        <div>
+          <p>Search</p>
+          <p>
+            Searching for: <span id="search-term"></span>
+          </p>
+          <input style={{ border: "1px solid black" }} onChange={onChange} />
+          <button onClick={onClick}>Reset Search</button>
+        </div>
+        <br />
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>City</th>
+              <th>Degree</th>
+              <th>Specialties</th>
+              <th>Years of Experience</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAdvocates.map((advocate) => {
+              return (
+                <tr key={advocate.id}>
+                  <td>{advocate.firstName}</td>
+                  <td>{advocate.lastName}</td>
+                  <td>{advocate.city}</td>
+                  <td>{advocate.degree}</td>
+                  <td>
+                    {advocate.specialties.map((s: string) => (
+                      <div key={s}>{s}</div>
+                    ))}
+                  </td>
+                  <td>{advocate.yearsOfExperience}</td>
+                  <td>{advocate.phoneNumber}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
+    </>
   );
 }
